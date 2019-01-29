aae0e2d8086a7985d0041977620533a9ef12f76c
Sebastian <sebastianecheverri900@gmail.com>
2019-01-28 21:26:32 -0500

    Crea la ruta POST /products, agrega la alerta para notice, agrega el boton de publicar producto en la lista de productos

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/products', to: 'products#index'
-  get '/products/new', to: 'products#new'
+  get '/products/new', to: 'products#new', as: 'new_product'
+  post '/products', to: 'products#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
